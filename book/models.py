5dfbeb9d09ee2798583d180d293695997b958d9d
hanna-parsa-80 <hanaparsa_80@yahoo.com>
2023-05-02 16:53:21 +0330

    processing

--- book/models.py
+++ book/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
+class Book(models.Model):
+    title= models.CharField(max_length=200, null=True)
+    Author= models.CharField(max_length=200, null=True)
+    Price= models.IntegerField()
+    Edition=models.integeField()
